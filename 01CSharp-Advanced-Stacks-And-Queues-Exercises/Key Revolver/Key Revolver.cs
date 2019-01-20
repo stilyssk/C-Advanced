e22015569e1dedb895ec29fab0337215c0cb8d8e
stilyssk <41970026+stilyssk@users.noreply.github.com>
2019-01-20 14:39:32 +0200

    Add Training 02

--- 01CSharp-Advanced-Stacks-And-Queues-Exercises/Key Revolver/Key Revolver.cs
+++ 01CSharp-Advanced-Stacks-And-Queues-Exercises/Key Revolver/Key Revolver.cs
@@ -6,7 +6,7 @@ namespace Key_Revolver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
---
+++ 02Multidimensional ArraysLab/02Multidimensional ArraysLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02Multidimensional_ArraysLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
