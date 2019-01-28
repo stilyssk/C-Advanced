fe2f78fc6ef198710651ecafd63524b66593be48
stilyssk <41970026+stilyssk@users.noreply.github.com>
2019-01-28 11:55:42 +0200

    04. CSharp-Advanced-Streams-Files-and-Directories-Lab

---
+++ 04.Streams-Files-and-Directories-Lab/Folder Size/Folder Size.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Folder_Size
+{
+    class Program
+    {
+        private static long GetDirectorySize(string folderPath)
+        {
+            DirectoryInfo di = new DirectoryInfo(folderPath);
+            return di.EnumerateFiles("*", SearchOption.TopDirectoryOnly).Sum(x => x.Length);
+
+            //di.EnumerateFiles("*", SearchOption.TopDirectoryOnly).Sum(fi => fi.Length);
+        }
+        static void Main(string[] args)
+        {
+            long folderSize = GetDirectorySize(@"../../../TestFolder");
+            Double test= folderSize/(Double)(1024*1024);
+            Console.WriteLine(test);
+        }
+    }
+}
--- 04.Streams-Files-and-Directories-Lab/Slice File/Slice File.cs
+++ 04.Streams-Files-and-Directories-Lab/Slice File/Slice File.cs
@@ -5,40 +5,56 @@ namespace Slice_File
 {
     class Program
     {
+        static void writeToFile (string nameOfFile,char[] tempBuffer)
+        {
+            StreamWriter streamFile = new StreamWriter(nameOfFile, true);
+            streamFile.Write(tempBuffer);
+
+            streamFile.Close();
+
+        }
         static void Main(string[] args)
         {
             StreamReader sliceMe = new StreamReader(@"sliceMe.txt");
             long filesize = sliceMe.BaseStream.Length;
-
-            int bufferSize = 4096;
-            char[] buffer  = new char[bufferSize];
-            int test;
+            int partSize = (int)filesize / 4;
+            int counterParts = 0;            
+            int currentPartSize;
             int count = 1;
             while (true)
             {
-                test = sliceMe.Read(buffer, 0, bufferSize);
-                if (test==0)
+                int bufferSize = 4096;
+                char[] buffer = new char[bufferSize];
+
+
+                if ((partSize-counterParts)<bufferSize)
                 {
-                    break;
+                    bufferSize = partSize - counterParts;
+                    currentPartSize = sliceMe.Read(buffer, 0, bufferSize);
+
+                    if (currentPartSize == 0)
+                    {
+                        break;
+                    }
+                    counterParts += currentPartSize;
+                    string nameOfFile = $"Part-{count}.txt";
+                    writeToFile(nameOfFile, buffer);
+                    count++;
+                    counterParts = 0;
                 }
-                StreamWriter streamFile = new StreamWriter($"Part-{count}.txt",true);
-                streamFile.Write(buffer);
-                streamFile.Close();
-                count++;
-                Console.WriteLine(string.Join("", buffer));
-                Console.WriteLine();
-                Console.WriteLine(test);
-                if (count>4)
+                else
                 {
-                    count = 1;
+                    currentPartSize = sliceMe.Read(buffer, 0, bufferSize);
+                    counterParts += currentPartSize;
+                    Console.WriteLine(currentPartSize);
+                    if (currentPartSize == 0||currentPartSize==1)
+                    {
+                        break;
+                    }
+                    string nameOfFile = $"Part-{count}.txt";
+                    writeToFile(nameOfFile, buffer);
+
                 }
-              
-             
-            
-
-          
-
-
             }
             sliceMe.Close();
             
