98c825456ef44745a21fe89ff022d7539f497233
stilyssk <41970026+stilyssk@users.noreply.github.com>
2019-01-30 21:09:23 +0200

    05. CSharp-Advanced-Functional-Programming-Lab

--- Functional Programming/Count Uppercase Words/Count Uppercase Words.cs
+++ Functional Programming/Count Uppercase Words/Count Uppercase Words.cs
@@ -13,13 +13,27 @@ namespace Count_Uppercase_Words
             List<string> inputLine = Console.ReadLine()
                 .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
                 .ToList();
-            Regex regexString = new Regex(@"[A-Z]\w+");
-        
-            List<string> outputResult = inputLine.Where(x=>regexString.Match(x).Success).ToList();
+            //Regex regexString = new Regex(@"[A-Z]\w+");
+            Func<string, bool> checkFirstLetter = CheckFirstUpperLetter;
+
+            List<string> outputResult = inputLine.Where(checkFirstLetter).ToList();
             foreach (var item in outputResult)
             {
                 Console.WriteLine(item);
             }
+         }
+        static bool CheckFirstUpperLetter(string inputString)
+        {
+            bool result = false;
+            for (char i = 'A'; i <='Z'; i++)
+            {
+                if (inputString[0]==i)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
         }
     }
 }
